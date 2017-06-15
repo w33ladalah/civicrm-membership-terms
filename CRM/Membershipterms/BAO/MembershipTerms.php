66d928384a200a3572b1e0c7ba2321ccbe9f2512
Hendro Wibowo <hendrothemail@gmail.com>
2017-06-15 21:02:22 +0700

    Rename BAO file

--- CRM/Membershipterms/BAO/MembershipTerms.php
+++ CRM/Membershipterms/BAO/MembershipTerms.php
@@ -1,6 +1,6 @@
 <?php
 
-class CRM_Membershipterms_BAO_MembershipsTerms extends CRM_Membershipterms_DAO_MembershipsTerms {
+class CRM_Membershipterms_BAO_MembershipTerms extends CRM_Membershipterms_DAO_MembershipTerms {
 
   /**
    * Create a new MembershipsTerms based on array-data
@@ -8,6 +8,7 @@ class CRM_Membershipterms_BAO_MembershipsTerms extends CRM_Membershipterms_DAO_M
    * @param array $params key-value pairs
    * @return CRM_Membershipterms_DAO_MembershipsTerms|NULL
    *
+   */
   public static function create($params) {
     $className = 'CRM_Membershipterms_DAO_MembershipsTerms';
     $entityName = 'MembershipsTerms';
@@ -20,6 +21,6 @@ class CRM_Membershipterms_BAO_MembershipsTerms extends CRM_Membershipterms_DAO_M
     CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
 
     return $instance;
-  } */
+  }
 
 }
