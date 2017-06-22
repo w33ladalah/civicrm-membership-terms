ce79ac9429e1bc3995dda30ea43753c7c6345db5
Hendro Wibowo <hendrothemail@gmail.com>
2017-06-22 16:17:38 +0700

    Rename entity files

--- xml/schema/CRM/Membershipterms/MembershipTerms.entityType.php
+++ xml/schema/CRM/Membershipterms/MembershipTerms.entityType.php
@@ -2,10 +2,10 @@
 // This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
 return array (
-  0 => 
+  0 =>
   array (
-    'name' => 'MembershipsTerms',
-    'class' => 'CRM_Membershipterms_DAO_MembershipsTerms',
-    'table' => 'civicrm_membershipsterms',
+    'name' => 'MembershipTerms',
+    'class' => 'CRM_Membershipterms_DAO_MembershipTerms',
+    'table' => 'civicrm_membership_terms',
   ),
 );
