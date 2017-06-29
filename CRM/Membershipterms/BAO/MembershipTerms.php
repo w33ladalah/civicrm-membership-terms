935ed2ff57de393d90dae9b50c5b64b223f2c9e5
Hendro Wibowo <hendrothemail@gmail.com>
2017-06-29 10:43:50 +0700

    Minor changes

--- CRM/Membershipterms/BAO/MembershipTerms.php
+++ CRM/Membershipterms/BAO/MembershipTerms.php
@@ -4,15 +4,15 @@ require_once 'CRM/Membershipterms/DAO/MembershipTerms.php';
 class CRM_Membershipterms_BAO_MembershipTerms extends CRM_Membershipterms_DAO_MembershipTerms {
 
   /**
-   * Create a new MembershipsTerms based on array-data
+   * Create a new MembershipTerms based on array-data
    *
    * @param array $params key-value pairs
-   * @return CRM_Membershipterms_DAO_MembershipsTerms|NULL
+   * @return CRM_Membershipterms_DAO_MembershipTerms|NULL
    *
    */
   public static function create($params) {
-    $className = 'CRM_Membershipterms_DAO_MembershipsTerms';
-    $entityName = 'MembershipsTerms';
+    $className = 'CRM_Membershipterms_DAO_MembershipTerms';
+    $entityName = 'MembershipTerms';
     $hook = empty($params['id']) ? 'create' : 'edit';
 
     CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
